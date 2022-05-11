1250ade028b33702d21be58c0aede9a8bfcbf314
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-05-11 17:17:39 -0300

    Agregar archivos de proyecto.

---
+++ tp-Juego/Personaje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_Juego
+{
+    abstract class Personaje
+    {
+        protected string Nombre;
+        protected int Fuerza;
+        protected int Agilidad;
+        protected int Magia;
+    }
+}
